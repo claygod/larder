31f13295c0a45a0feb16a5952bb0a9230bf360d8
claygod <claygod@yandex.ru>
2019-01-06 23:19:11 +0300

    Journal: change batcher->batcher.Client

--- journal/journal.go
+++ journal/journal.go
@@ -14,34 +14,16 @@ import (
 Journal - transactions logs saver (WAL).
 */
 type Journal struct {
-	//batcher      *batcher.Batcher
-	//batchChInput chan []byte
 	client *batcher.Client
 }
 
 func New(filePath string, alarmFunc func(error), chInput chan []byte, batchSize int) *Journal {
-	//f, _ := os.Create(filePath)
-	//b := batcher.NewBatcher(f, alarmFunc, chInput, batchSize)
 	clt, _ := batcher.Open(filePath, batchSize)
 	return &Journal{
-		//batcher:      b,
-		//batchChInput: chInput,
 		client: clt,
 	}
 }
 
-//func (j *Journal) Start() {
-//	j.batcher.Start()
-//}
-
-//func (j *Journal) Stop() {
-//	j.batcher.Stop()
-//}
-
 func (j *Journal) Write(toSave []byte) {
 	j.client.Write(toSave)
-
-	//	j.batchChInput <- toSave
-	//	ch := j.batcher.GetChan()
-	//	<-ch
 }
