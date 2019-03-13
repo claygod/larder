1ea7388a04287372b117fc4ad514a9129071e22b
Eduard <claygod@yandex.ru>
2019-03-13 21:40:27 +0300

    Add files via upload

--- larder.go
+++ larder.go
@@ -1,15 +1,14 @@
 package larder
 
 // Larder
-// API
+// API main
 // Copyright © 2018-2019 Eduard Sesigin. All rights reserved. Contacts: <claygod@yandex.ru>
 
 import (
 	//"bytes"
 	//"encoding/binary"
-	//"encoding/gob"
-	"bytes"
-	"fmt"
+	//"fmt"
+	//"time"
 	"sync"
 	"sync/atomic"
 
@@ -17,12 +16,6 @@ import (
 
 	"github.com/claygod/larder/journal"
 	"github.com/claygod/larder/repo"
-)
-
-const (
-	stateStopped int64 = iota
-	stateStarted
-	statePanic
 )
 
 /*
@@ -89,101 +82,6 @@ func (l *Larder) Stop() {
 }
 
 /*
-Write - записать ОДНУ запись в базу
-*/
-func (l *Larder) Write(key string, value []byte) error {
-	if atomic.LoadInt64(&l.hasp) != stateStarted {
-		return fmt.Errorf("Adding is possible only when the application started")
-
-	}
-	l.porter.Catch([]string{key})       // хватаем нужные записи (локаем)
-	defer l.porter.Throw([]string{key}) // бросаем по завершению (unlock)
-	defer l.checkPanic()                // при ошибке записи в журнал там возможна паника, её перехватывать
-
-	// проводим операцию  с inmemory хранилищем
-	l.store.setRecords(map[string][]byte{key: value})
-	// WAL: сформируем строку/строки для записи в WAL и заполним журнал
-	rec, err := l.prepareWriteToLog(codeWrite, key, value)
-	if err != nil {
-		return err
-	}
-	rec[0] = 0
-	//xxx := append(uint64ToBytes(uint64(len(rec))), rec...)
-	//TODO: необходимо в запись лога добавлять счётчик?
-	//TODO: необходимо в запись лога добавлять дату и/или время?
-	l.journal.Write(append(uint64ToBytes(uint64(len(rec))), rec...))
-	return nil
-}
-
-/*
-WriteList
-Важный момент - получая на вход мэп, мы гарантируем,
-что не будет две записи в один и тот же ключ.
-*/
-func (l *Larder) WriteList(input map[string][]byte) error {
-	if atomic.LoadInt64(&l.hasp) != stateStarted {
-		return fmt.Errorf("Adding is possible only when the application started")
-
-	}
-	keys := l.getKeysFromArray(input)
-	l.porter.Catch(keys)
-	defer l.porter.Throw(keys)
-	defer l.checkPanic() // при ошибке записи в журнал там возможна паника, её перехватывать
-
-	// проводим операцию  с inmemory хранилищем
-	l.store.setRecords(input)
-
-	//WAL
-	recs := make([][]byte, 0, len(input))
-	lenRecs := 0
-	for _, key := range keys {
-		rec, err := l.prepareWriteToLog(codeWrite, key, input[key])
-		if err != nil {
-			return err
-		}
-		recs = append(recs, rec)
-		lenRecs += len(rec)
-	}
-	l.journal.Write(append(uint64ToBytes(uint64(lenRecs)), bytes.Join(recs, []byte{})...))
-	return nil
-}
-
-func (l *Larder) Read(key string) ([]byte, error) {
-	if atomic.LoadInt64(&l.hasp) != stateStarted {
-		return nil, fmt.Errorf("Reading is possible only when the application started")
-
-	}
-	l.porter.Catch([]string{key})
-	defer l.porter.Throw([]string{key})
-	outs, err := l.store.getRecords([]string{key})
-	if err != nil {
-		return nil, err
-	}
-	return outs[key], nil
-}
-
-func (l *Larder) ReadList(keys []string) (map[string][]byte, error) {
-	if atomic.LoadInt64(&l.hasp) != stateStarted {
-		return nil, fmt.Errorf("Reading is possible only when the application started")
-
-	}
-	l.porter.Catch(keys)
-	defer l.porter.Throw(keys)
-	return l.store.getRecords(keys)
-}
-
-func (l *Larder) Delete(key string) error {
-	if atomic.LoadInt64(&l.hasp) != stateStarted {
-		return fmt.Errorf("Deleting is possible only when the application started")
-
-	}
-	l.porter.Catch([]string{key})
-	defer l.porter.Throw([]string{key})
-	//TODO:
-	return nil
-}
-
-/*
 SetHandler - add handler. This can be done both before launch and during database operation.
 */
 func (l *Larder) SetHandler(handlerName string, handlerMethod func([]string, Repo, interface{}) ([]byte, error)) error {
@@ -191,41 +89,4 @@ func (l *Larder) SetHandler(handlerName string, handlerMethod func([]string, Rep
 	//		return fmt.Errorf("Handles cannot be added while the application is running.")
 	//	}
 	return l.handlers.set(handlerName, handlerMethod)
-}
-
-/*
-Transaction - update of specified records, but not adding or deleting records.
-Arguments:
-- name of the handler for this transaction
-- keys of records that will participate in the transaction
-- additional arguments
-*/
-func (l *Larder) Transaction(handlerName string, keys []string, v interface{}) error {
-	if atomic.LoadInt64(&l.hasp) != stateStarted {
-		return fmt.Errorf("Transaction is possible only when the application started")
-
-	}
-	l.porter.Catch(keys)
-	defer l.porter.Throw(keys)
-	hdl, err := l.handlers.get(handlerName)
-	if err != nil {
-		return err
-	}
-	// проводим операцию  с inmemory хранилищем
-	toSave, err := l.store.transaction(keys, v, hdl) //TODO: тут нужно возвращать map[key]value с новыми значениями
-	if err != nil {
-		return err //l.log.Write(err)
-	}
-	//WAL
-	//TODO: сохранение изменённых записей (полученных после выполнения транзакции)
-	//TODO: сохранение аргументов транцакции, на основании которых сделаны изменения
-	l.journal.Write(toSave)
-	return nil
-}
-
-func (l *Larder) checkPanic() {
-	if err := recover(); err != nil {
-		atomic.StoreInt64(&l.hasp, statePanic)
-		fmt.Println(err)
-	}
 }
